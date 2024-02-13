f4d08142c44b4a403b1b14b1260d357e878c7dca
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-02-13 21:42:30 +0500

    task was done by Elyorbek

--- Product_API/Repositories/ProductRepository.cs
+++ Product_API/Repositories/ProductRepository.cs
@@ -1,25 +1,53 @@
-﻿using Product_API.Models;
+﻿using Npgsql;
+using Product_API.Models;
 
 namespace Product_API.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly List<Product> _products;
-
+        public string connectionString = "Host=localhost; Port = 5432; Database = TestCRUD; User Id = postgres; Password = Maqsudkhan;";
+        public NpgsqlConnection connection;
         public ProductRepository()
         {
-            _products = new List<Product>();
+            connection = new NpgsqlConnection(connectionString);
         }
 
         public Product Add(Product product)
         {
-            _products.Add(product);
+            connection.Open();
+            try
+            {
+                string query = $"insert into products(Name,Description,PhotoPath) values('{product.Name}','{product.Description}','{product.PhotoPath}');";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            connection.Close();
             return product;
         }
-
         public List<Product> GetAll()
         {
-            return _products;
+            List<Product> products = new List<Product>();
+            connection.Open();
+            try
+            {
+                string query = $"select * from products;";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                var result = cmd.ExecuteReader();
+                while (result.Read())
+                {
+                    products.Add(new Product { Id = Convert.ToInt32(result["Id"]), Name = Convert.ToString(result["Name"]), Description = Convert.ToString(result["Description"]), PhotoPath = Convert.ToString(result["PhotoPath"]) });
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            connection.Close();
+            return products;
         }
     }
 }
